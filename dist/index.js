1748de65d3ca88e553b6f03fcf94ce5afb84f7a1
Varunkumar Nagarajan <varunkumar.n@gmail.com>
2022-12-06 21:35:12 +0530

    Add logs

--- dist/index.js
+++ dist/index.js
@@ -10016,40 +10016,40 @@ const github = __nccwpck_require__(5438);
 const secrets = __nccwpck_require__(7975);
 
 try {
-  const raw = core.getInput("secrets");
-  const filename = core.getInput("file");
-  const pathname = core.getInput("path");
-  const mode = core.getInput("mode");
+  const raw = core.getInput('secrets');
+  const filename = core.getInput('file');
+  const pathname = core.getInput('path');
+  const mode = core.getInput('mode');
 
   const parsed = secrets.parse(raw.trim());
 
   if (parsed.length === 0) {
-    throw new Error("No secrets received");
+    throw new Error('No secrets received');
   }
 
-  core.setOutput("parsed", parsed);
+  core.setOutput('parsed', parsed);
 
   const fullpath = __nccwpck_require__.ab + "create-secrets-file/" + pathname + '/' + filename;
 
-  console.log("Secrets parsed");
-
-  core.setOutput("location", fullpath);
+  console.log('Secrets parsed');
+  console.log(fullpath);
+  core.setOutput('location', fullpath);
 
   const generated = secrets.format(parsed, mode);
 
   fs.writeFileSync(fullpath, generated);
 
-  console.log("Secrets file created");
+  console.log('Secrets file created');
 
-  console.log("Verifying...");
+  console.log('Verifying...');
 
   const found = secrets.search();
 
   const sanatized = secrets.sanatize(found);
 
-  core.setOutput("sanatized", sanatized);
+  core.setOutput('sanatized', sanatized);
 
-  core.setOutput("success", true);
+  core.setOutput('success', true);
 } catch (error) {
   core.setFailed(error.message);
 }
--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const core = require("@actions/core");
-const github = require("@actions/github");
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 // Another Package I've wrote
 // https://github.com/du5rte/secrets
-const secrets = require("secrets");
+const secrets = require('secrets');
 
 try {
-  const raw = core.getInput("secrets");
-  const filename = core.getInput("file");
-  const pathname = core.getInput("path");
-  const mode = core.getInput("mode");
+  const raw = core.getInput('secrets');
+  const filename = core.getInput('file');
+  const pathname = core.getInput('path');
+  const mode = core.getInput('mode');
 
   const parsed = secrets.parse(raw.trim());
 
   if (parsed.length === 0) {
-    throw new Error("No secrets received");
+    throw new Error('No secrets received');
   }
 
-  core.setOutput("parsed", parsed);
+  core.setOutput('parsed', parsed);
 
   const fullpath = path.resolve(__dirname, pathname, filename);
 
-  console.log("Secrets parsed");
-
-  core.setOutput("location", fullpath);
+  console.log('Secrets parsed');
+  console.log(fullpath);
+  core.setOutput('location', fullpath);
 
   const generated = secrets.format(parsed, mode);
 
   fs.writeFileSync(fullpath, generated);
 
-  console.log("Secrets file created");
+  console.log('Secrets file created');
 
-  console.log("Verifying...");
+  console.log('Verifying...');
 
   const found = secrets.search();
 
   const sanatized = secrets.sanatize(found);
 
-  core.setOutput("sanatized", sanatized);
+  core.setOutput('sanatized', sanatized);
 
-  core.setOutput("success", true);
+  core.setOutput('success', true);
 } catch (error) {
   core.setFailed(error.message);
 }
